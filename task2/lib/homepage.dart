e80e9ce703f01c5f28b6f7db6e002a06b0c9dfad
ahmadfk1227 <fafaroka578@gmail.com>
2023-08-10 23:38:59 +0300

    friday task ahmad farouk

---
+++ task2/lib/homepage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar:  AppBar(
+        backgroundColor: Colors.black,
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+
+        decoration: const BoxDecoration(
+             image : DecorationImage(
+              image: NetworkImage(
+                  'https://i.pinimg.com/564x/d8/4f/e5/d84fe564086a3ce6455a871dceb7db2b.jpg'
+              ) ,
+              fit: BoxFit.cover,
+
+            )
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+                  children:   [
+
+                    Image(
+                      image: AssetImage('images/1.png'),
+                      height : 220,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                    const Text(
+                      'ITI Quiz app',
+                      style: TextStyle(
+                        color: Colors.amberAccent,
+                        fontFamily: 'DancingScript',
+                        fontSize:40,
+
+
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'we are creative , enjoy our app',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'DancingScript',
+                        fontSize:27,
+
+
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        padding: EdgeInsets.only(left: 125,right:125)
+                      ),
+                        onPressed:( ){
+
+                        },
+
+                        child: Text('Start'
+                          ,style: TextStyle(
+                              fontSize: 20,
+                            )
+
+                        ),
+
+                    )
+
+
+                  ],
+
+      ),
+
+      ),
+
+    );
+
+
+
+
+
+
+   
+
+
+  }
+}
